df513a4cd58b143fc848ee6b65e9009538d79f79
hsntrk <hsk@mailbox.org>
2022-06-09 16:21:21 +0200

    ..

--- Hasan/Advanced/script.js
+++ Hasan/Advanced/script.js
@@ -1,34 +1,50 @@
 "use strict";
 class Vehicle {
-    constructor(model, yearproduction, fueltype, price, numberofseats, image) {
+    constructor(image, model, yearproduction, fueltype, numberofseats, price) {
+        this.image = image;
         this.model = model;
         this.yearproduction = yearproduction;
         this.fueltype = fueltype;
         this.numberofseats = numberofseats;
         this.price = price;
-        this.image = image;
+        array.push(this);
     }
     displayInfo() {
         return `<div class = "col"><div class="card" style="width: 18rem;">
   <img src="${this.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h3 class="card-title">Model: ${this.model}</h3>
-    <h5 class="card-title">Productionyear${this.yearproduction}</h5>
+    <h5 class="card-title">Production Year: ${this.yearproduction}</h5>
     <p class="card-text">Fueltype: ${this.fueltype}</p>
     <p class="card-text">Number of Seats: ${this.numberofseats}</p>
-    <h4 class="card-title">Productionyear${this.price}</h4>
+    <h4 class="card-title">List Price: ${this.price}</h4>
+    <button class="btn btn-info btncal">current price</button>
+    <h4 class="card-title currentprice">Current Price: <span class="result5">xx</span></h4>
     </div>
   </div>
 </div>`;
     }
+    calculate() {
+        return this.price * 0.8;
+    }
 }
-const cars = [];
-new Place("Hamburg", "It is a nice city", "hamburg.png");
-new Place("Nantes", "It is also nice", "nantes.jpeg");
-new Place("Stockholm", "It is a nice city", "stockholm.jpeg");
-new Place("Vitoria-Gasteiz", "It is also nice", "vitoria.jpg");
-new Theater("Sydney", "Landmark, skyline-dominating arts centre for opera, theatre, music and dance, plus guided tours.", "https://cdn.pixabay.com/photo/2014/06/06/09/36/sydney-opera-house-363244_1280.jpg", "Sydney Opera House", "+61292507111");
-new Theater("Buenos Aires", "This grand theater (circa 1908) known for acoustics hosts classical music, operas, ballets & tours.", "https://cdn.pixabay.com/photo/2015/01/09/17/34/opera-594592_1280.jpg", "Teatro Colón", "+541143787100");
-for (let place of cars) {
+const array = [];
+new Vehicle("images/audi_r8.jpg", "Audi R8", 2010, "benzin", 2, 78000);
+new Vehicle("images/audi_rs4.jpg", "Audi RS4", 2008, "benzin", 4, 88000);
+new Vehicle("images/audi_tt.jpg", "Audi TT", 2005, "benzin", 2, 45000);
+new Vehicle("images/bmw_1.jpg", "BMW 330i", 2011, "benzin", 4, 55000);
+new Vehicle("images/bmw_2.jpg", "BMW M3", 2011, "benzin", 2, 75000);
+new Vehicle("images/vw_r32.jpg", "Golf R32", 2004, "benzin", 4, 39000);
+for (let place of array) {
     document.getElementById("info").innerHTML += place.displayInfo();
+}
+// for (let val of array) {
+//   (document.getElementsByClassName("currentprice")[0] as HTMLElement).addEventListener("click", function() {
+//   })
+// }
+let btns = document.getElementsByClassName("btncal");
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        document.getElementsByClassName("result5")[i].innerHTML = array[i].calculate();
+    });
 }
